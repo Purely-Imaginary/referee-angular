6d3d481c3363966785ac79dc132a3a19a5ff7fc8
Amadeusz Opac <amadeusz.opac@dreamlab.pl>
2019-08-21 11:05:44 +0200

    download button status

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,13 +11,22 @@ export class SidebarComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
+  public buttonText = "Download"
+
   ngOnInit() {
   }
 
   refreshData() {
     const Http = new XMLHttpRequest();
     const url='http://localhost:3000/calculateMatches';
+    this.buttonText = "Downloading...";
+    const self = this;
     Http.open("GET", url);
+    Http.onload = function() {
+      const response = JSON.parse(Http.response);
+      const lastMatch = moment(response.lastParsedMatch).fromNow();
+      self.buttonText = response.matchesProcessed.toString() + " (" + lastMatch + ")";
+    };
     Http.send();
   }
 }
